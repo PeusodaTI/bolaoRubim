0327d290a41668b2c8040f3ddeb0774431f346bc
DESKTOP-1VV4LDO\Pedro <peusoda@hotmail.com>
2022-11-11 07:08:12 -0300

    Developing interface registration guesses

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,164 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './styles/main.css';
+
+import logo from '../public/logoT.png';
+import zap from '../public/zap.png';
+import tel from '../public/tel.png';
+import insta from '../public/insta.png';
+import cpR from '../public/cpR.png';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen flex flex-col">
+     
+      <div className="w-auto h-auto p-4">
+        <div className="flex flex-col items-center">
+          <img src={logo} alt="" className="h-14 w-14 left-10"></img>
+          <h1 className="text-lg font-bold mt-1 bg-clip-text text-transparent bg-gradient-to-r from-slate-900 via-slate-700 to-slate-800">
+            Bolão Esportivo Rubim
+          </h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      
+      <div className="w-auto h-auto p-2 flex-1 ">
+        <form className="p-2 ">
+          <div className="flex flex-col items-center mb-5">
+            <label className="text-white text-left text-dm mb-1">Nome</label>
+            <input type="text" className="bg-transparent border border-white rounded text-sm text-black text-center"/>
+            <label className="text-white text-left text-dm mt-2 mb-1">Telefone</label>
+            <input type="text" className="bg-transparent border-white border rounded text-sm text-black text-center"/>
+          </div>
+
+          <div className="flex justify-center mb-2">
+            <div className="w-1/3 h-auto text-center sm:text-right">
+              <label className="text-white text-dm">Cruzeiro</label>
+            </div>
+            <div className="flex-grow   space-x-1 sm:space-x-5 h-auto text-center">
+            <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+              <label className="text-white text-sm">X</label>
+              <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+            </div>
+            <div className="w-1/3 h-auto text-center sm:text-left">
+              <div className="">
+              <label className="text-white text-dm">Manchester City</label>
+            </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center mb-2">
+            <div className="w-1/3 h-auto text-center sm:text-right">
+              <label className="text-white text-dm">Cruzeiro</label>
+            </div>
+            <div className="flex-grow   space-x-1 sm:space-x-5 h-auto text-center">
+            <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+              <label className="text-white text-sm">X</label>
+              <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+            </div>
+            <div className="w-1/3 h-auto text-center sm:text-left">
+              <div className="">
+              <label className="text-white text-dm">Manchester City</label>
+            </div>
+            </div>
+          </div>
+          
+          <div className="flex justify-center mb-2">
+            <div className="w-1/3 h-auto text-center sm:text-right">
+              <label className="text-white text-dm">Cruzeiro</label>
+            </div>
+            <div className="flex-grow   space-x-1 sm:space-x-5 h-auto text-center">
+            <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+              <label className="text-white text-sm">X</label>
+              <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+            </div>
+            <div className="w-1/3 h-auto text-center sm:text-left">
+              <div className="">
+              <label className="text-white text-dm">Manchester City</label>
+            </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center mb-2">
+            <div className="w-1/3 h-auto text-center sm:text-right">
+              <label className="text-white text-dm">Cruzeiro</label>
+            </div>
+            <div className="flex-grow   space-x-1 sm:space-x-5 h-auto text-center">
+            <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+              <label className="text-white text-sm">X</label>
+              <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+            </div>
+            <div className="w-1/3 h-auto text-center sm:text-left">
+              <div className="">
+              <label className="text-white text-dm">Manchester City</label>
+            </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center mb-2">
+            <div className="w-1/3 h-auto text-center sm:text-right">
+              <label className="text-white text-dm">Cruzeiro</label>
+            </div>
+            <div className="flex-grow   space-x-1 sm:space-x-5 h-auto text-center">
+            <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+              <label className="text-white text-sm">X</label>
+              <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+            </div>
+            <div className="w-1/3 h-auto text-center sm:text-left">
+              <div className="">
+              <label className="text-white text-dm">Manchester City</label>
+            </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center mb-2">
+            <div className="w-1/3 h-auto text-center sm:text-right">
+              <label className="text-white text-dm">Cruzeiro</label>
+            </div>
+            <div className="flex-grow   space-x-1 sm:space-x-5 h-auto text-center">
+            <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+              <label className="text-white text-sm">X</label>
+              <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+            </div>
+            <div className="w-1/3 h-auto text-center sm:text-left">
+              <div className="">
+              <label className="text-white text-dm">Manchester City</label>
+            </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center mb-2">
+            <div className="w-1/3 h-auto text-center sm:text-right">
+              <label className="text-white text-dm">Cruzeiro</label>
+            </div>
+            <div className="flex-grow   space-x-1 sm:space-x-5 h-auto text-center">
+            <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+              <label className="text-white text-sm">X</label>
+              <input type="number" className="sm:text-right text-sm text-center backdrop-blur-sm w-8 rounded bg-transparent border border-white text-black"/>
+            </div>
+            <div className="w-1/3 h-auto text-center sm:text-left">
+              <div className="">
+              <label className="text-white text-dm">Manchester City</label>
+            </div>
+            </div>
+          </div>
+
+          <div className="flex justify-center mt-4 mb-3">
+            <button className="rounded bg-indigo-200 p-1 text-mm mt-6">Cadastrar Palpite</button>
+          </div>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <div className="w-auto h-auto p-2">
+        <div className="flex justify-center space-x-20 mt-4 ">
+          <img src={tel} alt="" className="w-6 h-6 "></img>
+          <img src={zap} alt="" className="w-6 h-6 "></img>
+          <img src={insta} alt="" className="w-6 h-6 "></img>
+        </div>
+        <div className="flex justify-center items-center space-x-2 mt-4">
+          <img src={cpR} alt="" className="w-3 h-3"></img>
+          <span className="text-white text-mm">Copyright 2022 PHDevelopment</span>      
+        </div>
+      </div>
+
     </div>
   )
 }
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx', 
+    './index.html',
+  ],
+  container: {
+    width: '100%',
+  },
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    fontSize: {
+      mm: '12px',
+      sm: '13px',
+      dm: '14px'
+    },
+    extend: {
+      backgroundImage: {
+        'futebol': "url('/soccer.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
